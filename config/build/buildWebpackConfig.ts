d27c255c31af95a737fa6202398648db5e05b5f4
Kirill Kalinin <kalinin1507.2012@gmail.com>
2024-01-31 20:10:33 +0400

    initial commit: настроен вебпак конфиг, тс конфиг, базовый роутинг, код сплиттинг, смена темы

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buidResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
+
+  return {
+    mode: mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buidResolvers(),
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined
+  };
+}
