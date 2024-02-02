fc85e1e8b510c138f8eadb66386742f172b0f3bb
Kirill Kalinin <kalinin1507.2012@gmail.com>
2024-02-02 22:57:53 +0400

    i18n интернализация

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,20 @@ import { AppRouter } from "./providers/router";
 import { NavBar } from "widgets/NavBar";
 import { useTheme } from "./providers/ThemeProvider";
 import { SideBar } from "widgets/SideBar";
+import { Suspense } from "react";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <NavBar />
-      <div className="content-page">
-        <SideBar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="">
+        <NavBar />
+        <div className="content-page">
+          <SideBar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next"
+
 const MainPage = () => {
+  const {t} = useTranslation()
   return (
-    <div>MainPage</div>
+    <div>{t('Главная')}</div>
   )
 }
 
